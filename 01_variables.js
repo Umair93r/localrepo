b9b2e6c7209fa9ab9b2be66c268341cb9c9b65b4
Umair93r <rangrezumair286@gmail.com>
2026-05-27 12:12:33 +0530

    more practice

---
+++ 01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 144553
+let accountEmail = "rangrezumair286@gmail.com"
+var accountPassword = "1234"
+accountCity = "jaipur"
+let accountState;
+
+// accountId = 2 // not allowed 
+
+
+ accountEmail = "rangrezumair286@gmail.com "
+ accountPassword = "21212121"
+ accountCity = "Aurangabad"
+
+console.log(accountId);
+console.table([accountPassword,accountEmail,accountCity,accountState]);
+
+/* 
+ prefer not to use var
+ becouse issue of block scope and functional scope
+*/
