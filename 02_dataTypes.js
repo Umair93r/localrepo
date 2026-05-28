c6b781b11e3ae1ebe28bcef0344c7cbd06101302
Umair93r <rangrezumair286@gmail.com>
2026-05-28 19:58:26 +0530

    update projet

--- 02_dataTypes.js
+++ 02_dataTypes.js
@@ -14,3 +14,11 @@ console.log(typeof isLoggedIn);
 
 let valueInNumber = Number(score);
 console.log(valueInNumber);
+//---------------------------------------//
+
+
+let value1 = 234;
+let value2 = 234;
+
+console.log(value1+value2);
+
