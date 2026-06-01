a18c8eba4bdf3aac5aa2a86b411ceb599265bd95
Umair93r <rangrezumair286@gmail.com>
2026-06-01 12:29:33 +0530

    file update

--- loops.js
+++ loops.js
@@ -1,5 +1,9 @@
-let num = 10;
-while (num >= 20) {
-  num = num + 1;
-  console.log(num);
+let num = 10; // Initialize a variable named num with the value 10
+while (num >= 20) {  // Start a while loop that continues as long as num is greater than or equal to 20
+  num = num + 1; // Increment the value of num by 1 in each iteration
+  console.log(num); // Print the current value of num to the console
+}
+
+for(let i = 1; i <= 5; i++){ // Start a for loop that initializes i to 1, continues as long as i is less than or equal to 5, and increments i by 1 in each iteration
+    console.log(i); // Print the current value of i to the console
 }
