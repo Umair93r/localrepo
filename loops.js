43b45fb9202182528e9351f6a3d830a266aed15c
Umair93r <rangrezumair286@gmail.com>
2026-06-01 12:39:09 +0530

    update

--- loops.js
+++ loops.js
@@ -4,6 +4,13 @@ while (num >= 20) {  // Start a while loop that continues as long as num is grea
   console.log(num); // Print the current value of num to the console
 }
 
-for(let i = 1; i <= 5; i++){ // Start a for loop that initializes i to 1, continues as long as i is less than or equal to 5, and increments i by 1 in each iteration
-    console.log(i); // Print the current value of i to the console
+for(let i = 1; i <= 5; i++){   // Start a for loop that initializes i to 1, continues as long as i is less than or equal to 5, and increments i by 1 in each iteration
+    console.log(i);  // Print the current value of i to the console
+}
+
+let x = 0;
+while(x < 1){
+  console.log(x);
+  x = Math.random() * 2;
+
 }
