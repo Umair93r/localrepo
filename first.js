f2194d7232be704d8280ad263e8d859d49b695cd
Umair93r <rangrezumair286@gmail.com>
2026-06-02 13:19:15 +0530

    add some code

--- first.js
+++ first.js
@@ -1,31 +1,48 @@
-function bankAccount() {
-  let balance = 1000;
+// function bankAccount() {
+//   let balance = 1000;
 
-return{
-  deposit(amount){
-    balance += amount;
-    console.log(balance);
-  },
+// return{
+//   deposit(amount){
+//     balance += amount;
+//     console.log(balance);
+//   },
 
-  getBalance() {
-    console.log(balance);
-  }
+//   getBalance() {
+//     console.log(balance);
+//   }
   
-};
+// };
 
-}
+// }
 
-const amount = bankAccount();
-console.log(account.balance)
+// const amount = bankAccount();
+// console.log(account.balance)
 
 //----------------------------------------------------------------------//
 
 function init() {
   var name = "Mozilla"; 
   function displayName() {
-   
-    console.log(name); 
+   console.log("too inner",name); 
   }
   displayName();
 }
 init();
+
+//-------------------------------------------------------------//
+
+function outer(){
+  let username = "umair";
+  console.log("outer" , secret)
+  function inner(){
+    let secret = "my123"
+    console.log("inner", username);
+  }
+  function innerTwo(){
+    console.log("innerTwo", username);
+    console.log(secret)
+  }
+  inner();
+  innerTwo();
+}
+outer();
