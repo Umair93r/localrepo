dc584ae770ebcf993c3f353d967b82c807a10838
Umair93r <rangrezumair286@gmail.com>
2026-06-03 11:05:01 +0530

    update

--- closures.js
+++ closures.js
@@ -1,55 +1,55 @@
 function greet(name) {
-  return function(){
-   console.log("hello " + name);
-   
-  }
-    
+  return function () {
+    console.log("hello " + name);
   };
-  greet("Umair")();
+}
+greet("Umair")();
 
-  //------------------------------------------//
+//------------------------------------------//
 
-  function createCounter(counter) {
-    let count = 0;
+function createCounter(counter) {
+  let count = 0;
 
-    return function(){
-       count++;
-       console.log(count);
-       
-    };
-  }
+  return function () {
+    count++;
+    console.log(count);
+  };
+}
 
-  const counter = createCounter();
+const counter = createCounter();
 
-  counter();
+counter();
 
-  //----------------------------------------------//
+//----------------------------------------------//
 
-  function bank() {
-    let balance = 100000;
+function bank() {
+  let balance = 100000;
 
-    return {
-      deposit(amount){
-        balance += amount;
-        console.log(balance);
-        
-      },
+  return {
+    deposit(amount) {
+      balance += amount;
+      console.log(balance);
+    },
 
-      withdraw(amount){
-        balance -= amount;
-        console.log(balance);
-        
-      },
+    withdraw(amount) {
+      balance -= amount;
+      console.log(balance);
+    },
 
-      checkBalance(){
-        console.log(balance);
-        
-      }
-    }
-  }
+    checkBalance() {
+      console.log(balance);
+    },
+  };
+}
 
-  const account = bank ();
+const account = bank();
 
-  account.deposit(5000);
-  account.withdraw(2000);
-  account.checkBalance()
+account.deposit(5000);
+account.withdraw(2000);
+account.checkBalance();
+
+
+
+function isValidScheama(obj) {
+  return obj;
+}
