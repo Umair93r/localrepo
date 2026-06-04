254f0f3c64d54d3157766cfd3e5883b741bc75bf
Umair93r <rangrezumair286@gmail.com>
2026-06-04 13:51:11 +0530

    add some file and changes

---
+++ array.js
@@ -0,0 +1,13 @@
+let foodItems =["potato", "apple", "litchi", "tomato"];
+
+
+foodItems.push("chips");
+
+foodItems.pop("chips")
+
+toString(foodItems);
+
+
+console.log(foodItems);
+
+console.log("deleted", deletedItem);
