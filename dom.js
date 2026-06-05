6d2bb0002e31cd4a87b2a204ea9a365f97b04377
Umair93r <rangrezumair286@gmail.com>
2026-06-05 23:43:16 +0530

    new file

---
+++ dom.js
@@ -0,0 +1,5 @@
+function changeText() {
+  document.getElementById("title").innerText = "Welcome!"; // innerText is used to change the text content of an element
+}
+
+// document.querySelector(".box") modern way to select an element by query selector
